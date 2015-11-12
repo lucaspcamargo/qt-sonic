79d0a476fe5928398c6eae1a28ac23ee949b1476
Lucas P. Camargo <lucas.p.camargo@grad.ufsc.br>
2015-11-12 20:19:21 -0200

    Removed mob objs from version control

--- android/obj/local/x86/objs/OpenAL-MOB/Alc/mixer_neon.o.d
+++
@@ -1,5 +0,0 @@
-/home/camargo/github/dw/android/obj/local/x86/objs/OpenAL-MOB/Alc/mixer_neon.o: \
- /home/camargo/github/dw/android/jni/Alc/mixer_neon.c \
- /home/camargo/github/dw/android/jni/config-oal.h
-
-/home/camargo/github/dw/android/jni/config-oal.h:
